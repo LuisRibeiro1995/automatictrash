54f00416186cb7e2e9f3a7cdf5c0f0b778b388a2
Siham <46928466+siham87@users.noreply.github.com>
2019-01-30 11:14:50 +0100

    Add files via upload

---
+++ PoubelleAuto.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+#include <HCSR04.h>
+UltraSonicDistanceSensor distanceSensor(8, 7);
+Servo servoMotor;
+
+void setup() {
+  
+  Serial.begin(9600);
+  servoMotor.attach(10);
+}
+
+void loop() {
+  
+  double distance = distanceSensor.measureDistanceCm();
+
+  Serial.println(distance);
+  
+  if(distance < 30  && distance > 0){
+    servoMotor.write(80); 
+    delay(5000);
+  }else{
+    
+ servoMotor.write(10);
+  
+  }
+  
+
+}
